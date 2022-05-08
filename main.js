6000f3ce68faf01177582598a965d516e2c90f6a
jinnkimm7 <jinnkimm7@gmail.com>
2022-05-08 22:07:36 +0900

    JS : Navbar
    change the color

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.clientHeight;
 document.addEventListener('scroll', () => {
   if (window.scrollY > navbarHeight) {
-    navbar.classList.add('navbar--blue');
+    navbar.classList.add('navbar--dark');
   } else {
-    navbar.classList.remove('navbar--blue')
+    navbar.classList.remove('navbar--dark')
   }
 })
 
